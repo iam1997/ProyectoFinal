b256b18414fa85f843b79069fba56af4172d9f91
iam1997 <manuel.wereb@outlook.de>
2021-11-02 06:56:20 +0100

    Agregué la vista aprende

--- app/src/main/java/com/example/proyectofinal/AprendeFragment.kt
+++ app/src/main/java/com/example/proyectofinal/AprendeFragment.kt
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.proyectofinal.databinding.FragmentAprendeBinding
 import com.example.proyectofinal.databinding.FragmentCrearCuentaBinding
-import com.example.proyectofinal.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,15 +16,15 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [AprendeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class AprendeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAprendeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,18 +40,10 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAprendeBinding.inflate(inflater, container, false)
 
-        binding.buttonAprendeHome.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_aprendeFragment)
-        }
-
-        binding.buttonCerrarSessionHome.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
-        }
-
-        binding.buttonRecursosHome.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_recursosFragment)
+        binding.buttonVolverAprende.setOnClickListener {
+            findNavController().navigate(R.id.action_aprendeFragment_to_homeFragment)
         }
 
         return binding.root
@@ -64,12 +56,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment AprendeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            AprendeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
